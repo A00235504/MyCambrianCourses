e9c77309c973a19f79c343cfc912c0863cce5815
aakashsadnani <A00235504@mycambrian.ca>
2021-07-01 22:39:57 -0400

    Firebase get data from database to recyclerview done

---
+++ app/src/main/java/com/aakash/mycambriancourses/CoursesListActivity.java
@@ -0,0 +1,68 @@
+package com.aakash.mycambriancourses;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class CoursesListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    RecyclerViewAdapter
+            adapter; // Create Object of the Adapter class
+    DatabaseReference mbase; // Create object of the
+    // Firebase Realtime Database
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses_list);
+
+        mbase
+                = FirebaseDatabase.getInstance().getReference().child("Courses");
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(this));
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<Person> options
+                = new FirebaseRecyclerOptions.Builder<Person>()
+                .setQuery(mbase, Person.class)
+                .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new RecyclerViewAdapter(options);
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter);
+    }
+
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override protected void onStart()
+    {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stoping of the activity
+    @Override protected void onStop()
+    {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
